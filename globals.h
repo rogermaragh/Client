20b4a99642a1b4957137b4644985d39aac1dcd7d
Roger Maragh <redirection@gmail.com>
2026-03-03 20:00:53 -0500

    Create globals.h with global variable declarations

---
+++ globals.h
@@ -0,0 +1,11 @@
+// globals.h
+
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+// Global variable declarations with g_ prefix
+int g_globalInteger;
+double g_globalDouble;
+char g_globalChar;
+
+#endif // GLOBALS_H
